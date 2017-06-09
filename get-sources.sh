d99b2c1e6985b6e08a3d22e7403fe2ec7aee7a1e
mozhijun <1059224309@qq.com>
2017-06-09 09:44:40 +0800

    add adbwin64

---
+++ get-sources.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#         DEPENDENCIES
+# -------------------------
+wget https://www.openssl.org/source/openssl-1.0.2j.tar.gz
+tar -xvzf openssl-1.0.2j.tar.gz
+
+#         CONFIG
+# -------------------------
+
+# Branch to checkout from Android source code repo
+branch=android-7.0.0_r5
+
+# DOWNLOAD necessary files
+# -------------------------
+echo "\n>> >>> ADB for ARM <<< \n"
+echo "\n>> Downloading necessay files ($branch branch)\n"
+mkdir android-adb
+cd android-adb
+mkdir system
+cd system
+git clone -b $branch https://android.googlesource.com/platform/system/core
+git clone -b $branch https://android.googlesource.com/platform/system/extras
+cd ..
+mkdir external
+cd external
+git clone -b $branch https://android.googlesource.com/platform/external/zlib
+git clone -b $branch https://android.googlesource.com/platform/external/openssl
+git clone -b $branch https://android.googlesource.com/platform/external/libselinux
+git clone -b $branch https://android.googlesource.com/platform/external/zopfli/
+git clone -b $branch https://android.googlesource.com/platform/external/safe-iop/
+cd ..
+mkdir platform
+cd platform
+git clone -b $branch https://android.googlesource.com/platform/development/
+git clone -b $branch https://android.googlesource.com/platform/build/
+mkdir frameworks
+cd frameworks
+git clone -b $branch https://android.googlesource.com/platform/frameworks/base/
